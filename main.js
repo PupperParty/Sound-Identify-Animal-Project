12d9a74763b3b1a7cc382c1e05e0ece73b52dda6
PupperParty <93052348+PupperParty@users.noreply.github.com>
2022-04-24 14:07:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
+
 function startClassification(){
     navigator.mediaDevices.getUserMedia({audio:true});
     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/js90P5l-3/model.json',modelReady);
 }
-function gotResults(){
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results)
+{
+if(error) {
+    console.error(error);
+}
+else{
+    console.log(results);
+    random_number_r = Math.floor(Math.random()* 255) +1;
+    random_number_g = Math.floor(Math.random()* 255) +1;
+    random_number_b = Math.floor(Math.random()* 255) +1;
     
+    document.getElementById("result_label").innerHTML = 'I can hear-' + results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy-' + (results[0].confidence*100).toFixed(2) +"%";
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+", "+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+", "+random_number_g+","+random_number_r+")";
+    img= document.getElementById('dog')
+    img1= document.getElementById('whale')
+    img2= document.getElementById('elephant')
+    img3= document.getElementById('sheep')
+
+    if(results[0].label == "Background Noise"){
+        img.src = 'dog.gif';
+        img1.src = 'whale.jpg';
+        img2.src = 'elephant.jpg';
+        img3.src = 'sheep.jpg';
+    }
+    else if(results[0].label == "whale song"){
+        img.src = 'dog.jpg';
+        img1.src = 'whale.gif';
+        img2.src = 'elephant.jpg';
+        img3.src = 'sheep.jpg';
+    }
+    else if(results[0].label == "elephant"){
+        img.src = 'dog.jpg';
+        img1.src = 'whale.jpg';
+        img2.src = 'elephant.gif';
+        img3.src = 'sheep.jpg';
+    }
+    else{
+        img.src = 'dog.jpg';
+        img1.src = 'whale.jpg';
+        img2.src = 'elephant.jpg';
+        img3.src = 'sheep.gif';
+    }
+}
 }
